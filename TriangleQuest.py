dbeedff24b75d311979c50632839cf7e7aceb9db
AshwinR1402 <aashwinr1402@gmail.com>
2025-09-16 19:15:41 +0530

    Update TriangleQuest.py
    How it works

--- TriangleQuest.py
+++ TriangleQuest.py
@@ -3,4 +3,4 @@ for i in range(1,int(input())):
 """How? Let us break it into three segment for each iteration.
 10**i gives the value of   10 to the power i (ie.10^i) .At i=1(10^i gives value 10)
 (10**i)//9 gives the remainder 1,11,111..... until N=10 
-i*((10**i)//9) gives i*1,i*11,i*111,.....
+i*((10**i)//9) gives i*1,i*11,i*111,....."""
