3da03546d2b249d6e4271a8abcd2c58637d10217
AshwinR1402 <aashwinr1402@gmail.com>
2025-10-01 14:34:07 +0530

    Update Co-Ordinates.py
    Updated

--- Co-Ordinates.py
+++ Co-Ordinates.py
@@ -2,12 +2,5 @@ x=int(input())
 y=int(input())
 z=int(input())
 n=int(input())
-d=[]
-d1=[[x1,y1,z1] for x1 in range(x+1) for y1 in range(y+1) for z1 in range(z+1)]
-for a in d1:
-    i=0
-    for b in a:
-        i+=b
-    if i!=n:
-        d.append(a)      
+d=[[x1,y1,z1] for x1 in range(x+1) for y1 in range(y+1) for z1 in range(z+1) if x1+y1+z1!=n]      
 print(d)
